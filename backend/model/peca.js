b5f3de4ef66b2a78b088f8ad6632613a1808d295
Marcos Motta <mrcsmotta1@gmail.com>
2020-05-19 14:56:19 -0300

    Criado Model para cadastro das peças fabricadas

---
+++ backend/model/peca.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const PecaSchema = new mongoose.Schema({
+  //nome descricao peso data fabricaca os producao
+  os: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  nome: {
+    type: String,
+    required: true,
+  },
+  descricao: {
+    type: String,
+    required: true,
+  },
+  peso: {
+    type: mongoose.Schema.Types.Decimal128,
+    required: true,
+  },
+  datafabricacao: {
+    type: Date,
+    required: true,
+  },
+  updated: {
+    type: Date,
+    default: Date.now,
+  },
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Peca = mongoose.model("Peca", PecaSchema);
+
+module.exports = Peca;
