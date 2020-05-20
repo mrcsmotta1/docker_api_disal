665053d2e61a68914c1293bfe03173708b871ed7
Marcos Motta <mrcsmotta1@gmail.com>
2020-05-20 18:18:04 -0300

    Listagem de peças

--- backend/src/app/controllers/PecaController.js
+++ backend/src/app/controllers/PecaController.js
@@ -21,20 +21,15 @@ router.post("/register", async (req, res) => {
   }
 });
 
-//list
-router.post("/register", async (req, res) => {
+//listagem
+router.get("/registers", async (req, res) => {
   try {
-    const peca = await Peca.create(req.body);
-    console.log("peca: " + peca);
+    const peca = await Peca.find();
     return peca == null
       ? res.status(404).json({ message: "peca not found" })
       : res.status(201).send(peca);
   } catch (e) {
-    if (e.code == 11000) {
-      return res.status(409).json({ error: "Duplicate record" });
-    }
-
-    return res.status(400).json({ error: "Registration failed" });
+    return res.status(400).json({ error: "Select failed" });
   }
 });
 
